afbc078e8018ae21369983eff90b05d56992f601
Thirunavukkarasu Muthusamy <tmuthu201@apac.comcast.com>
2017-10-09 10:49:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+var Connection = require('tedious').Connection;  
+var fs   = require("fs");
+var mkdirp = require("mkdirp");
+var config = {  
+    userName: 'workmonitor',  
+    password: 'work@123',  
+    server: '52.32.4.177',  
+    // If you are on Microsoft Azure, you need this:  
+    options: {encrypt: true, database: 'workMonitoringV1', rowCollectionOnDone: true}  
+};  
+var connection = new Connection(config);  
+connection.on('connect', function(err) {  
+    // If no error, then good to proceed.  
+    console.log("Connected");  
+    executeStatement();  
+});  
+
+var Request = require('tedious').Request;  
+var TYPES = require('tedious').TYPES;  
+
+function executeStatement() {  
+    request = new Request("select *from INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'", function(err) {  
+    if (err) {  
+        console.log(err);}  
+    });  
+
+    request.on('doneInProc', function(rowCount, more, rows) {  
+        var tablesList = [];
+        rows.forEach(function(row) {  
+            tablesList.push(row[2]);
+        });  
+        generateDirectories(tablesList);
+    });  
+    connection.execSql(request);  
+}  
+
+function generateDirectories(tablesList){
+    generateClientCode(tablesList);
+    generateServerCode(tablesList);
+}
+
+function generateClientCode(tablesList){
+    tablesList.forEach(function(table){
+        var path = "client/"+table.value;
+        mkdirp(path, function(err){
+            if(err) return err;
+            fs.writeFile(path+"/test.js","File content");
+        });
+    });
+}
+
+function generateServerCode(tablesList){
+    tablesList.forEach(function(table){
+        var path = "server/"+table.value;
+        mkdirp(path, function(err){
+            if(err) return err;
+            fs.writeFile(path+"/test.java","File content");
+        });
+    });
+}
